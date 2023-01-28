d8ece47e44900c0b2355d955d9f978797ded3118
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-01-28 11:46:26 +0530

    Update structure.sql

--- Student Management/structure.sql
+++ Student Management/structure.sql
@@ -7,3 +7,19 @@ CREATE TABLE IF NOT EXISTS student_data
       city char(30),
       branch char(34)     
     )
+
+
+
+CREATE TABLE IF NOT EXISTS student_marks
+      (
+        tid int primary key AUTO_INCREMENT,
+        sid int NOT NULL , 
+        s_roll int ,
+        branch char(34) ,
+        pass_year int , 
+        total_marks int 
+      )
+      
+      
+      
+      
